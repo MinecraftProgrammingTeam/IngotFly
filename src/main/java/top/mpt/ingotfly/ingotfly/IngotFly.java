4a3a94561719c4dda98eafaa629ef2e45c450fc1
You_M <1055965862@qq.com>
2022-12-27 16:08:15 +0800

    Init commit

---
+++ src/main/java/top/mpt/ingotfly/ingotfly/IngotFly.java
@@ -0,0 +1,11 @@
+package top.mpt.ingotfly.ingotfly;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * @author You_M
+ */
+public class IngotFly implements ModInitializer {
+    public void onInitialize() {
+    }
+}
